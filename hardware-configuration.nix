def589a4c030c34c49019a73a5dfca737f5d0daf
Piorum <90207649+Piorum@users.noreply.github.com>
2025-11-07 18:43:36 -0700

    12

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -24,18 +24,19 @@
       options = [ "fmask=0022" "dmask=0022" ];
     };
 
+  fileSystems."/mnt/Samsung9100PRO" =
+    { device = "/dev/disk/by-uuid/0403559f-d182-4b76-82da-e2a823037ccd";
+      fsType = "ext4";
+    };
+
+  fileSystems."/mnt/Samsung980" =
+    { device = "/dev/disk/by-uuid/b8be3ff5-beb9-407a-b024-2e05e58508c7";
+      fsType = "ext4";
+    };
+
   swapDevices =
     [ { device = "/dev/disk/by-uuid/2e107e27-5e15-4754-b742-a784dc00868e"; }
     ];
-
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp5s0.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp6s0.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp13s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
