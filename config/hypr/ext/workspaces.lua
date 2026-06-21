9fe8dce5b01417b82dc0afb2df59775626fe4a8e
Piorum <90207649+Piorum@users.noreply.github.com>
2026-06-21 13:46:44 -0600

    workspaces fix

--- config/hypr/ext/workspaces.lua
+++ config/hypr/ext/workspaces.lua
@@ -5,10 +5,10 @@ for i = 1, 10 do
     hl.workspace_rule({ workspace = tostring(i), monitor = "DP-4" })
 end
 for i = 11, 20 do
-    hl.workspace_rule({ workspace = tostring(i), monitor = "DP-4" })
+    hl.workspace_rule({ workspace = tostring(i), monitor = "DP-5" })
 end
 for i = 21, 30 do
-    hl.workspace_rule({ workspace = tostring(i), monitor = "DP-4" })
+    hl.workspace_rule({ workspace = tostring(i), monitor = "DP-6" })
 end
 
 -- define relative binds
